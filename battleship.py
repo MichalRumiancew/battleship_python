2517cb2ec28bda7f799e56a438febc593f543cf5
Michal Rumiancew <47053566+MichalRumiancew@users.noreply.github.com>
2020-10-08 21:52:29 +0200

    Add files via upload

---
+++ battleship.py
@@ -0,0 +1,103 @@
+
+import random
+import os
+
+# board = []
+# # wielkość
+# for i in range(0,5):
+#     board.append(["o"] * 5)
+
+# def print_board(board):
+#     for i in board:
+#         print(i)
+
+# print_board(board)
+
+
+os.system("cls || clear")
+
+
+def init_board():
+    board = [['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ]]
+    print(board)
+    board2 = [['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ]]
+    print(board2)
+
+
+
+       
+
+
+def get_move(board, player):
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    return row, col
+
+
+def get_ai_move(board, player):
+    """Returns the coordinates of a valid move for player on board."""
+    row, col = 0, 0
+    return row, col
+
+
+def mark(board, player, row, col):
+    """Marks the element at row & col on the board for player."""
+    pass
+
+
+def has_won(board, player):
+    """Returns True if player has won the game."""
+    return False
+
+
+def is_full(board):
+    """Returns True if board is full."""
+    return False
+
+
+def print_board(board):
+    """Prints a 3-by-3 board on the screen with borders."""
+    board = [['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ]]
+    print("\n")
+    print("  1" + "2" + "3" + "4" + "5")
+    print("A " + board[0][0] + board[0][1] + board[0][2] + board[0][3] + board[0][4]) 
+    print("B " + board[1][0] + board[1][1] + board[1][2] + board[1][3] + board[1][4])
+    print("D " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("E " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("F " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("\n")
+    board2 = [['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ],['O','O','O','O','O' ]]
+    print("\n")
+    print("  1" + "2" + "3" + "4" + "5")
+    print("A " + board[0][0] + board[0][1] + board[0][2] + board[0][3] + board[0][4]) 
+    print("B " + board[1][0] + board[1][1] + board[1][2] + board[1][3] + board[1][4])
+    print("D " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("E " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("F " + board[2][0] + board[2][1] + board[2][2] + board[2][3] + board[2][4])
+    print("\n")
+
+   
+def print_result(winner):
+    """Congratulates winner or proclaims tie (if winner equals zero)."""
+    pass
+
+
+def battleship_game(mode='HUMAN-HUMAN'):
+    board = init_board()
+
+    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
+    print_board(board)
+    row, col = get_move(board, 1)
+    mark(board, 1, row, col)
+
+    winner = 0
+    print_result(winner)
+
+
+def main_menu():
+    battleship_game('HUMAN-HUMAN')
+
+
+if __name__ == '__main__':
+    main_menu()
+    
